f6d49062291bd19c45a43b0c9351787f02a58882
jason.li@isharework.com <jason.li@isharework.com>
2019-10-10 10:50:51 +1100

    change index.js to server.js

--- server.js
+++ server.js
@@ -16,19 +16,19 @@ const app = express();
 //     origin: true,
 //     methods: ["POST","GET","PUT","DELETE","OPTIONS"],
 //   };
-// app.use(cors(issue2options));
-// app.options('*', cors());
+app.use(cors());
+//app.options('*', cors());
 
-app.all("*", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,x-access-token"
-    );
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    if (req.method == "OPTIONS") res.sendStatus(200);
-    /*让options请求快速返回*/ else next();
-  });
+// app.all("*", function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,x-access-token"
+//     );
+//     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//     if (req.method == "OPTIONS") res.sendStatus(200);
+//     /*让options请求快速返回*/ else next();
+//   });
 
 
 mongoose.connect(
